f61480624c90f2366039b3318be1eb108025fc1d
Tom Durrant <tom_durrant@waters.com>
2020-05-13 11:31:40 +0100

    Showing action injection

--- Complex-Sample-App/src/TennisBookings.Web/Areas/Admin/Controllers/CourtsController.cs
+++ Complex-Sample-App/src/TennisBookings.Web/Areas/Admin/Controllers/CourtsController.cs
@@ -16,12 +16,10 @@ namespace TennisBookings.Web.Areas.Admin.Controllers
     public class CourtsController : Controller
     {
         private readonly ICourtBookingService _courtBookingService;
-        private readonly ICourtMaintenanceService _courtMaintenanceService;
 
-        public CourtsController(ICourtBookingService courtBookingService, ICourtMaintenanceService courtMaintenanceService)
+        public CourtsController(ICourtBookingService courtBookingService)
         {
             _courtBookingService = courtBookingService;
-            _courtMaintenanceService = courtMaintenanceService;
         }
         
         [HttpGet]
@@ -88,9 +86,9 @@ namespace TennisBookings.Web.Areas.Admin.Controllers
         }
 
         [Route("Maintenance/Upcoming")]
-        public async Task<ActionResult> UpcomingMaintenance()
+        public async Task<ActionResult> UpcomingMaintenance([FromServices] ICourtMaintenanceService courtMaintenanceService)
         {
-            var maintenanceSchedules = await _courtMaintenanceService.GetUpcomingMaintenance();
+            var maintenanceSchedules = await courtMaintenanceService.GetUpcomingMaintenance();
 
             var maintenanceViewModels = maintenanceSchedules.Select(x => new CourtMaintenanceViewModel
             {
